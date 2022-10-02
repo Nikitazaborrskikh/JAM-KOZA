09b930aa97126f808ebf0d2d2e2d948cec366b7d
ThatOneDude0 <99406502+ThatOneDude0@users.noreply.github.com>
2022-10-02 14:06:23 +0300

    Anim
    anim

---
+++ Assets/UIscripts/OpenShop.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenShop : MonoBehaviour
+{
+    public Transform box;
+    public CanvasGroup Back;
+
+    private void OnEnable()
+    {
+        Back.alpha = 0;
+        Back.LeanAlpha(1, 0.5f);
+
+        box.localPosition = new Vector2(0, -Screen.width);
+        box.LeanMoveLocalY(0, 0.5f).setEaseOutExpo().delay = 0.1f;
+    }
+
+
+    public void CloseShop()
+    {
+        Back.LeanAlpha(0, 0.5f);
+        box.LeanMoveLocalY(-Screen.width, 0.5f).setEaseInExpo().setOnComplete(Complete);
+    }
+
+    void Complete()
+    {
+        gameObject.SetActive(false);
+    }
+}
