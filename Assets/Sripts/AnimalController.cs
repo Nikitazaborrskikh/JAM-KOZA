7c50bcfc90f8820eff725e100f119f0cafebea8a
Nikitazaborrskikh <zaborskikh03@gmail.com>
2022-10-02 12:54:10 +0300

    AnimalScript

--- Assets/Sripts/AnimalController.cs
+++ Assets/Sripts/AnimalController.cs
@@ -10,7 +10,10 @@ public class AnimalController : MonoBehaviour
     [SerializeField] private Slider hp;
     [SerializeField] private Slider time;
     [SerializeField] private GameObject _text;
+    [SerializeField] private GameObject  _button;
     private bool _buttonIspressed = false;
+    private bool timedOut = false;
+    
 
     public void Start()
     {
@@ -20,15 +23,24 @@ public class AnimalController : MonoBehaviour
     IEnumerator WannaEat()
     {
         _text.SetActive(true);
-        while (_buttonIspressed == false)
+        _button.SetActive(true);
+
+        while(true)
         {
-            time.value -= 0.01f;
+            
+            if (time.value == 0)
+            { 
+                hp.value -= 1f;
+                continue;
+            }
+
+            time.value--;
+
+
+
         }
         
-        if (time.value == 0f)
-        {
-            hp.value -= 1f;
-        }
+        
         yield return new WaitUntil((() => _buttonIspressed == true || time.value == 0));
         StartCoroutine(Idle());
     }
@@ -36,16 +48,43 @@ public class AnimalController : MonoBehaviour
     IEnumerator Idle()
     {
         _text.SetActive(false);
+        _button.SetActive(false);
         _buttonIspressed = false;
         time.value = time.maxValue;
         yield return new WaitForSeconds(1f);
         StartCoroutine(WannaEat());
     }
 
+    private IEnumerator TimeoutChecker(float timeout)
+    {
+        timedOut = false;
+        while (timeout > 0)
+        {
+            timeout -= 0.01f;
+            if (timeout <= 0)
+            {
+                timedOut = true;
+                StopCoroutine(TimeoutChecker(time.value));
+                
+                yield return null;
+                
+                
+            }
+        }
+
+        
+        
+        
+    }
     public void Feed()
     {
         _buttonIspressed = true;
         
         StartCoroutine(Idle());
+    }
+
+    private void Update()
+    {
+        Debug.Log(time.value);
     }
 }
