0332d23ff47cbdd04166e5144e6d498ba87e93db
ThatOneDude0 <99406502+ThatOneDude0@users.noreply.github.com>
2022-10-03 12:31:32 +0300

    ReadyShop
    RS

---
+++ Assets/UIscripts/Shops/ShopTwo.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopTwo : MonoBehaviour
+{
+    [SerializeField] private Image _animal;
+    [SerializeField] private Transform _room;
+    [SerializeField] protected GameObject _buttonBuy;
+
+    private void Update()
+    {
+
+    }
+
+    public void OnBuyAnimal()
+    {
+        Instantiate(_animal, _room.position, Quaternion.identity);
+        _buttonBuy.SetActive(false);
+    }
+
+    public void DeathAnimal()
+    {
+        _buttonBuy.SetActive(true);
+    }
+}
